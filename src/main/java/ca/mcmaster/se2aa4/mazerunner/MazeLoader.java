770c0603750dbbe8b3b01dc5a5ccfb5a6a7d2534
Midhousha <anuram@mcmaster.ca>
2025-01-24 23:08:19 -0500

    Displays the maze file in readable format #1

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeLoader.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeLoader.java
@@ -5,7 +5,34 @@ import java.io.*;
 public class MazeLoader {
 
     public static void loadMaze(String filePath) {
-        // Skeleton method to load maze from the file
-        System.out.println("Loading maze from file: " + filePath);
+
+    public static char[][] loadMaze(String filePath) {
+        char[][] maze = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int rows = 0;
+            while ((line = reader.readLine()) != null) {
+                rows++;
+            }
+
+            reader.close();
+            reader = new BufferedReader(new FileReader(filePath));
+
+            String firstLine = reader.readLine();
+            int columns = firstLine.length();
+            maze = new char[rows][columns];
+
+            int row = 0;
+            maze[row] = firstLine.toCharArray();
+            while ((line = reader.readLine()) != null) {
+                row++;
+                maze[row] = line.toCharArray();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error reading maze file: " + e.getMessage());
+        }
+
+        return maze;
     }
 }
