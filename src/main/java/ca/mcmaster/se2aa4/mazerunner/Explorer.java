eaa3bb0bc627f2143bcff63c8f4f0fba14b28525
Midhousha <anuram@mcmaster.ca>
2025-02-02 23:37:27 -0500

    Update to position tracking #10

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Explorer.java
@@ -1,44 +1,31 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public class Explorer {
-    private int x;
-    private int y;
+    private Position position;
     private Direction direction;
 
-    // Constructor to initialize position and direction
-    public Explorer(int startX, int startY, Direction startDirection) {
-        this.x = startX;
-        this.y = startY;
+    public Explorer(Position startPosition, Direction startDirection) {
+        this.position = startPosition;
         this.direction = startDirection;
     }
 
     public void moveForward() {
-        switch (direction) {
-            case NORTH:
-                y--;
-                break;
-            case EAST:
-                x++;
-                break;
-            case SOUTH:
-                y++;
-                break;
-            case WEST:
-                x--;
-                break;
-        }
-        printPosition();
+        position = position.move(direction);
     }
 
     public void turnLeft() {
-        direction = Direction.values()[(direction.ordinal() + 3) % 4];
+        direction = direction.turnLeft();
     }
 
     public void turnRight() {
-        direction = Direction.values()[(direction.ordinal() + 1) % 4];
+        direction = direction.turnRight();
     }
 
-    public void printPosition() {
-        System.out.println("Explorer is at (" + x + ", " + y + "), facing " + direction);
+    public Position getPosition() {
+        return position;
+    }
+
+    public Direction getDirection() {
+        return direction;
     }
 }
