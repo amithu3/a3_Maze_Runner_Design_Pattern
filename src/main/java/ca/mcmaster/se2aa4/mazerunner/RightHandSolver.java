eb9ed5010f42e943a8638504cb0229fc87348212
Midhousha <anuram@mcmaster.ca>
2025-02-02 23:51:18 -0500

    Added right-hand wall-following algorithm #13

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandSolver.java
@@ -0,0 +1,25 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class RightHandSolver implements MazeSolver {
+
+    @Override
+    public String solve(Maze maze) {
+        Position pos = maze.findEntry();
+        Direction dir = Direction.RIGHT;
+        StringBuilder path = new StringBuilder();
+
+        while (!pos.equals(maze.findExit())) {
+            if (maze.isValidPosition(pos.move(dir.turnRight()))) {
+                dir = dir.turnRight();
+                path.append('R');
+            } else if (maze.isValidPosition(pos.move(dir))) {
+                path.append('F');
+            } else {
+                dir = dir.turnLeft();
+                path.append('L');
+            }
+            pos = pos.move(dir);
+        }
+        return path.toString();
+    }
+}
